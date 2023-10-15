7991f587915c26cb33648b92a46ade304ef1750f
IT21806490 <it21806490@my.sliit.lk>
2023-10-16 02:21:06 +0530

    Import useRef

--- frontend/src/component/GetTours.js
+++ frontend/src/component/GetTours.js
@@ -1,5 +1,5 @@
 import "../App.css";
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import {Link} from "react-router-dom";
 import axios from "axios";
 import ToursAndRoutePlanning from "./ToursAndRoutePlanningNav";
