6bcaec1aaeefc2880c53aab0d045f34b1b9725dc
Thisal Dilmith <IT21815850@my.sliit.lk>
2023-10-17 10:35:26 +0530

    pdf

--- frontend/src/Components/VehicleOwnerManagement.js
+++ frontend/src/Components/VehicleOwnerManagement.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFileCsv, faEye, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faFilePdf, faEye, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import backgroundImage from './1096643.jpg';
-import Papa from 'papaparse';
+import { PDFDocument, rgb } from 'pdf-lib';
 import Swal from 'sweetalert2';
+
 
 export default function VehicleOwnerManagement() {
   const [searchQuery, setSearchQuery] = useState('');
@@ -32,34 +33,96 @@ export default function VehicleOwnerManagement() {
     owner.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
-  const createCsv = () => {
-    const csvData = filteredOwners.map(owner => ({
-      Name: owner.name,
-      NIC: owner.nic ? owner.nic.toString() : '',
-      Location: owner.location,
-      Email: owner.email,
-      Contact: owner.contact ? owner.contact.toString() : ''
-    }));
+  const createPdf = async () => {
+    const pdfDoc = await PDFDocument.create();
+    const page = pdfDoc.addPage();
   
-    const csv = Papa.unparse(csvData);
-    const csvDataUri = `data:text/csv;charset=utf-8,${encodeURIComponent(csv)}`;
+    const { width, height } = page.getSize();
   
+    const title = 'Vehicle Owners Detail'; // Add your title here
+  
+    const titleSize = 20;
+    const textSize = 11;
+    const titleColor = rgb(0, 0, 0);
+    const textColor = rgb(0, 0, 0);
+    const spacing = 20;
+  
+    let y = height - 50;
+  
+    const titleWidth = titleSize * title.length / 1.5; // Adjust the factor to position the title correctly
+    const titleX = (width - titleWidth) / 2;
+  
+    page.drawText(title, {
+      x: titleX,
+      y,
+      size: titleSize,
+      color: titleColor,
+    });
+    y -= spacing * 2; // Increase the spacing after the title
+  
+    filteredOwners.forEach((owner) => {
+      const { name, location, nic, email, contact } = owner;
+  
+      page.drawText(`Name: ${name}`, {
+        x: 50,
+        y,
+        size: textSize,
+        color: textColor,
+      });
+      y -= spacing;
+  
+      page.drawText(`NIC: ${nic}`, {
+        x: 50,
+        y,
+        size: textSize,
+        color: textColor,
+      });
+      y -= spacing;
+  
+      page.drawText(`Location: ${location}`, {
+        x: 50,
+        y,
+        size: textSize,
+        color: textColor,
+      });
+      y -= spacing;
+  
+      page.drawText(`Email: ${email}`, {
+        x: 50,
+        y,
+        size: textSize,
+        color: textColor,
+      });
+      y -= spacing;
+  
+      page.drawText(`Contact: ${contact}`, {
+        x: 50,
+        y,
+        size: textSize,
+        color: textColor,
+      });
+      y -= spacing;
+  
+      y -= spacing;
+    });
+  
+    const pdfBytes = await pdfDoc.save();
+  
+    const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+    const url = URL.createObjectURL(blob);
     const link = document.createElement('a');
-    link.setAttribute('href', csvDataUri);
-    link.setAttribute('download', 'VehicleOwnersReport.csv');
-    document.body.appendChild(link);
+    link.href = url;
+    link.download = 'VehicleOwnersReport.pdf';
     link.click();
+  
     Swal.fire({
       icon: 'success',
-      title: 'CSV file generated!',
-      text: 'The CSV file has been successfully generated and downloaded.',
+      title: 'PDF file generated!',
+      text: 'The PDF file has been successfully generated and downloaded.',
     });
-
   };
-  
-    
-  
 
+ 
   return (
     <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundImage: `url(${backgroundImage})`, backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed', backgroundSize: 'cover' }}>
       <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundImage: `url(${backgroundImage})`, backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed', backgroundSize: 'cover', overflow: 'auto' }}>
@@ -88,13 +151,9 @@ export default function VehicleOwnerManagement() {
                     </div>
                   </div>
                   <div className="col-1">
-                    <button
-                      className="btn btn-primary col-12"
-                      onClick={createCsv}
-                      style={{ backgroundColor: 'green', color: 'white' }}
-                    >
-                      <FontAwesomeIcon icon={faFileCsv} />
-                    </button>
+                  <button className="btn btn-secondary col-12" onClick={createPdf}>
+                                    <FontAwesomeIcon icon={faFilePdf} />
+                                </button>
 
                   </div>
                   <div className="col-1">
